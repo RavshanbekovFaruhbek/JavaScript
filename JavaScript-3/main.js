e48a8a2f98fff67f5caf0a10608391cc422b8111
RavshanbekovFaruhbek <ravshanbekovfaruxbek@gmail.com>
2026-04-17 12:01:57 +0500

    Add files via upload

---
+++ JavaScript-3/main.js
@@ -0,0 +1,16 @@
+// let name = "Farruxbek"
+// console.log(name+ " "+name.length)
+// console.log(name[0])
+// console.log(name.indexOf("F"))
+// console.log(name.lastIndexOf("u"))
+// console.log(name.includes("Farruxbek"))
+
+
+let til = "JavaScript"
+console.log(til.slice(0, 4))
+console.log(til.slice(-6))
+console.log(til.toUpperCase())
+
+
+let msg = "Assalom sen yomon odomsan va yomon do'st"
+console.log(msg.replaceAll("yomon" , "yaxshi"))
