7299e69f6226488b2fff86dfc802ffa6d448f564
RavshanbekovFaruhbek <ravshanbekovfaruxbek@gmail.com>
2026-04-20 11:31:45 +0500

    Add files via upload

---
+++ JavaScript-4/main.js
@@ -0,0 +1,14 @@
+let name = "Farruxbek"
+console.log(name+ " " + name.length)
+console.log(name.toUpperCase())
+
+let ism = "JavaScript juda qiziqarli va kuchli dasturlash tilidir"
+console.log(ism.indexOf("juda"))
+console.log(ism.includes("juda"))
+
+let pochta = "ali_uzb@gmail.com"
+console.log(pochta.slice("-10"))
+
+let meva = "Olma , Behi , Uzum"
+console.log(meva.trim())
+console.log(meva.replace("Olma" , "Behi"))
