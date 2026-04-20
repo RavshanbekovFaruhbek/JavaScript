2a4100017c0d400471d875653b421ad577dbc9fa
RavshanbekovFaruhbek <ravshanbekovfaruxbek@gmail.com>
2026-04-20 11:56:18 +0500

    Add files via upload

---
+++ JavaScript-5/main.js
@@ -0,0 +1,12 @@
+let son = 34.34534534
+console.log(son , typeof son)
+console.log(son , typeof son.toString())
+console.log(son.toFixed(2) , typeof son)
+
+const text = prompt("Son ")
+console.log(parseInt(text) , typeof parseInt(text))
+
+console.log(Number("10.5"))
+console.log(parseInt("10.5px"))
+console.log(parseFloat("10.5px"))
+
