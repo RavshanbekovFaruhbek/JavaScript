f6133c54e6e99ce8c7966193686abcd0e196b4da
RavshanbekovFaruhbek <ravshanbekovfaruxbek@gmail.com>
2026-04-22 11:57:13 +0500

    Add files via upload

---
+++ JavaScript_6/main.js
@@ -0,0 +1,9 @@
+let name = "   aLISHER   "
+name = name.trim()
+console.log(name.charAt(0).toUpperCase() + name.slice(1).toLowerCase())
+
+
+
+let tel = "+998 (90) 123 45 67"
+console.log(tel.replace("(90)" , "90"))
+
