b6594927b2c2498c99eee93c9ffd83adeb83a2ea
RavshanbekovFaruhbek <ravshanbekovfaruxbek@gmail.com>
2026-05-01 11:57:04 +0500

    Add files via upload

---
+++ JavaScript-8/main.js
@@ -0,0 +1,12 @@
+let mevalar = ["olma" , "banan" , "anor" , "limon" , "apelsin" , "qulupnay"]
+let number = new Array([1,3,4])
+let string = Array.from("abc")
+let mashinalar = ["jentra" , "malibu" , "damas" , "nexia" , "tico" , "swm" , "li9" , "bivaydi" , "nexia 3" , "prado"]
+console.log(mevalar[3])
+console.log(number)
+console.log(string)
+console.log(mevalar.length)
+console.log(mevalar[mevalar.length -1])
+console.log(mashinalar.length)
+console.log(mashinalar)
+
