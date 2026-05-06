a7eb6b56d9fdf0cdf13d7a1be7748fdc6031c23f
RavshanbekovFaruhbek <ravshanbekovfaruxbek@gmail.com>
2026-05-05 22:57:27 -0800

    Add files via upload

---
+++ JavaScript-9/index./main.js
@@ -0,0 +1,13 @@
+// let name = "Ali"
+// function salomBer(){
+//     console.log("Salom " + name)
+// }
+// salomBer()
+
+// function sonlar(s){
+//     return prompt
+// }
+// let a = Number(prompt("Birinchi son"))
+// let b = Number(prompt("Ikkinchi son"))
+// console.log(a + b)
+
