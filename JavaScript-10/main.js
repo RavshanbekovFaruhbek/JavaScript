03afb9eee0cbd915253dd22a6f38168dd39647bf
RavshanbekovFaruhbek <ravshanbekovfaruxbek@gmail.com>
2026-05-05 22:57:44 -0800

    Add files via upload

---
+++ JavaScript-10/main.js
@@ -0,0 +1,26 @@
+// function salomlash(){
+//     return prompt
+// }
+// let ism = ("Ali")
+// console.log("Xayrli tong , " + ism)
+
+// let belgiSoni = ("Farruxbek")
+// console.log(belgiSoni.length)
+
+// let shifrla = ("assalom")
+// console.log(shifrla.replaceAll("a" , "@"))
+
+// const chegirma = (narx, foiz) => narx - (narx * foiz / 100)
+// console.log(chegirma(70000, 20))
+
+// let yetkazibBerish = prompt("Nechaming boldi")
+// if(yetkazibBerish > 1000){
+//     console.log("Yetkazib berish bepul")
+// }else{
+//     console.log("Yetkazib berish 20 ming")
+// }
+
+function muzqaymoqNarxi(soni){
+    return soni * 5000
+}
+console.log(muzqaymoqNarxi(Number(prompt("Muzqaymoq qancha"))))
